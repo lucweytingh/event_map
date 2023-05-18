d408205e8bdc4d9080165f02d160f9231d277af6
Luc Weytingh <lucweytingh321@gmail.com>
2023-05-18 22:17:02 +0200

    Updated readme
    [Ticket: #X]

--- README.md
+++
@@ -1,2 +0,0 @@
-# events_map
-To create a map view for available events scraped from some websites
--- scripts/open_event_map.py
+++ scripts/open_event_map.py
@@ -1,11 +1,9 @@
 from src.location_mapper import LocationMapper
 from src.echtzeitmusik import EchtZeitMusik
 
-
 ezm = EchtZeitMusik()
-lm = LocationMapper()
-
 names, descriptions, addresses, coordinates = ezm.get_events()
 
+lm = LocationMapper()
 lm.create_map(names, descriptions, addresses, coordinates)
 lm.open_map()
